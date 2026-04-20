1bdf447602b61b151db9130ff0fec4a7bfaebbe8
sourov <sourovsh533@gmail.com>
2026-04-20 22:26:30 +0600

    remove blog section from app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,16 @@
-import { BlogSection } from "@/components/blog-section";
 import { ContactSection } from "@/components/contact-section";
 import { ExperienceSection } from "@/components/experience-section";
 import { Hero } from "@/components/hero";
 import { ProjectsSection } from "@/components/projects-section";
 import { SkillsSection } from "@/components/skills-section";
-import { getAllBlogPosts } from "@/lib/blog";
 
-export default async function Home() {
-  const posts = await getAllBlogPosts();
-
+export default function Home() {
   return (
     <main className="space-y-30">
       <Hero />
       <SkillsSection />
       <ExperienceSection />
       <ProjectsSection />
-      <BlogSection posts={posts.slice(0, 4)} />
       <ContactSection />
     </main>
   );
